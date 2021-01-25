5f558ff186d4680bd56fa291440542d2a5c6415c
orhanors <orsorhan1@gmail.com>
2021-01-25 23:08:31 +0300

    experience edited

--- src/models/experience/index.js
+++ src/models/experience/index.js
@@ -1,19 +1,19 @@
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
 
 const experienceSchema = new mongoose.Schema(
-    {   username: { type: String },
-        role: { type: String },
-        company: { type: String },
-        startDate: { type: Date, default: Date.now},
-        endDate: { type: Date, default: Date.now },
-        description: { type: String },
-        area: { type: String },
-        image: { type: String }
-    },
-    { timestamps: true },
+	{
+		user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+		role: { type: String },
+		company: { type: String },
+		startDate: { type: Date, default: Date.now },
+		endDate: { type: Date, default: Date.now },
+		description: { type: String },
+		area: { type: String },
+		image: { type: String },
+	},
+	{ timestamps: true }
 );
 
-const Experience = mongoose.model("Experience", experienceSchema)
+const Experience = mongoose.model("Experience", experienceSchema);
 
-module.exports = Experience
+module.exports = Experience;
