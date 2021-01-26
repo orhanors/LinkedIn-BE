6fc218230764044b905f9f62107abbdc6401a875
zenocoz <federico.soncini@gmail.com>
2021-01-26 10:54:06 +0100

    delete comments

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -93,16 +93,3 @@ exports.postImage = async (req, res, next) => {
     console.log(error)
   }
 }
-// - GET https://yourapi.herokuapp.com/api/posts/
-// Retrieve posts
-// - POST https://yourapi.herokuapp.com/api/posts/
-// Creates a new post  DONE
-// - GET https://yourapi.herokuapp.com/api/posts/{postId}
-// Retrieves the specified post
-// - PUT https://yourapi.herokuapp.com/api/posts/{postId}
-// Edit a given post
-// - DELETE https://yourapi.herokuapp.com/api/posts/{postId}
-// Removes a post
-// - POST https://yourapi.herokuapp.com/api/posts/{postId}
-// Add an image to the post under the name of "post"
-// #EXTRA: Find a way to return also the user with the posts, in order to have the Name / Picture to show it correcly on the frontend
